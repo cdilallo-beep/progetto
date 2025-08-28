92bc963a3d2f59368e3b698064aac4d67877b1aa
Massimo Pollutri <massimopollutri@gmail.com>
2025-08-28 16:00:10 +0200

    Initial commit with project setup

--- Prodotto.java
+++ Prodotto.java
@@ -1,3 +1,31 @@
 public class Prodotto {
+    private String nome;
+    private double prezzoBase;
+
+    public Prodotto(String nome, double prezzo) {
+        this.nome = nome;
+        this.prezzoBase = prezzo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPrezzo() {
+        return prezzoBase;
+    }
+
+    public void dettagliProdotto() {
+        System.out.println("Nome: " + nome + ", Prezzo: " + prezzoBase + " euro");
+    }
+
+    public void setNome(String nuovoNome) {
+        this.nome = nuovoNome;
+    }
+
+    public void setPrezzo(double nuovoPrezzo) {
+        this.prezzoBase = nuovoPrezzo;
+    }
+
     
 }
