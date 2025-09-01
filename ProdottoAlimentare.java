d69576136b07f59fdc460d96b12dda0bb63659ae
rzavanelli-blip <rzavanelli@itsmeccanicabruzzo.eu>
2025-09-01 09:10:58 +0200

    Add files via upload

--- ProdottoAlimentare.java
+++ ProdottoAlimentare.java
@@ -1,3 +1,19 @@
-public class ProdottoAlimentare {
-    
+public class ProdottoAlimentare extends Prodotto {
+
+    private int giorniAllaScadenza;
+
+    public ProdottoAlimentare(String nome, double prezzoBase, int giorniAllaScadenza) {
+        super(nome, prezzoBase);
+        this.giorniAllaScadenza = giorniAllaScadenza;
+    }
+
+    @Override
+    public double calcolaPrezzoFinale() {
+        if (giorniAllaScadenza <= 3) {
+            return getPrezzoBase() * 0.8; // sconto 20%
+        }
+        return getPrezzoBase();
+    }
 }
+
+
