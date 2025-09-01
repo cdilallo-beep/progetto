59eed964b3162fcef1c500a575b3d81ed79d00b5
cdilallo-beep <cdilallo@itsmeccanicabruzzo.eu>
2025-09-01 09:13:13 +0200

    Add files via upload

--- ProdottoElettronico.java
+++ ProdottoElettronico.java
@@ -1,3 +1,12 @@
-public class ProdottoElettronico {
-    
+public ProdottoElettronico(String nome, double prezzoBase, boolean garanzia) {
+    super(nome, prezzoBase);
+    this.garanzia = garanzia;
+}
+
+@Override
+public double calcolaPrezzoFinale() {
+    if (garanzia) {
+        return prezzoBase * 1.10; 
+    }
+    return prezzoBase;
 }
